62272df8bd82e34c6b797681b33d2411a6219b5a
Vlad Khitev <vkhitev@gmail.com>
2017-04-13 14:38:53 +0300

    Created basic routes

--- app.js
+++ app.js
@@ -4,15 +4,48 @@ const path = require('path')
 
 const express = require('express')
 const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const routes = require('./app_server/routes')
 
 const app = express()
 
-const router = require('./app_server/routes')
+app.set('views', path.join(__dirname, 'app_server', 'views'))
+app.set('view engine', 'ejs')
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', router)
+app.use('/', routes)
 
-app.listen(3000, () => {
-  console.log('Started on http://localhost:3000')
+app.use((req, res, next) => {
+  const err = new Error('Not found')
+  err.status = 404
+  next(err)
 })
+
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+module.exports = app
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,21 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send('app_server/routes/index.js')
+  res.render('index', {
+    message: 'Main page'
+  })
+})
+
+router.get('/admin', (req, res) => {
+  res.render('admin', {
+    message: 'Admin page'
+  })
+})
+
+router.get('/post/:postid', (req, res) => {
+  res.render('post', {
+    message: 'Post page ' + req.params.postid
+  })
 })
 
 module.exports = router
