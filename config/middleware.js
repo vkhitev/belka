1ae6146575582444652ffa7ef3f56eeaba7693a9
Vlad Khitev <vkhitev@gmail.com>
2017-04-21 22:44:12 +0300

    Login page, session is almost done

--- config/middleware.js
+++ config/middleware.js
@@ -5,6 +5,7 @@ const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 
 module.exports = function (app) {
   app.use(favicon(path.join('web-server', 'public', 'favicon.svg')))
@@ -13,6 +14,11 @@ module.exports = function (app) {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(cookieParser())
   app.use(express.static(path.join('web-server', 'public')))
+  app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+  }))
 
   return app
 }
--- web-server/routes/index.js
+++ web-server/routes/index.js
@@ -2,11 +2,26 @@ const { Router } = require('express')
 
 const ctrl = require('../controllers/main-controller')
 
+function auth (req, res, next) {
+  if (req.session &&
+      req.session.user === 'admin' &&
+      req.session.admin) {
+    return next()
+  } else {
+    // return res.sendStatus(401)
+    res.redirect('/login')
+  }
+}
+
 const router = Router()
 
 router.get('/', ctrl.renderHomepage)
 router.get('/posts', ctrl.renderPosts)
 router.get('/post/:postid', ctrl.renderPost)
-router.get('/admin', ctrl.renderAdmin)
+
+router.get('/login', ctrl.renderLogin)
+router.post('/login', ctrl.postLogin)
+router.get('/logout', ctrl.logout)
+router.get('/admin', auth, ctrl.renderAdmin)
 
 module.exports = router
