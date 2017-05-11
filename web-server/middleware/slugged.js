6dd55f3a28f331a424f35bd69081e27bdc9f0ee4
Vlad Khitev <vkhitev@gmail.com>
2017-05-11 15:33:17 +0300

    Added slug middleware

---
+++ web-server/middleware/slugged.js
@@ -0,0 +1,36 @@
+const fetchData = require('../controllers/fetch-data')
+const fmt = require('../controllers/formatters')
+
+// Todo: middleware between route and handler
+function slugged (action, route, handler) {
+  function addSlug (path, id, handler) {
+    return function (req, res) {
+      fetchData({
+        url: `${path}/${req.params[id]}`,
+        attributes: ['name'],
+        transform: {
+          name: fmt.slugifyOne
+        }
+      }).then(slug => {
+        if (req.params.slug === slug.name) {
+          handler(req, res)
+        } else {
+          res.redirect(`/${path}/${req.params[id]}/${slug.name}`)
+        }
+      })
+    }
+  }
+  const path = route.match(/\/(.+)\//)[1]
+  const id = route.match(/:(.+)/)[1]
+  action([route, route + '/:slug'], addSlug(path, id, handler))
+}
+
+// renderPost (req, res) {
+//     foo(req, res, 'posts', 'postid', renderPostSlug)
+//   },
+
+//   renderCategoryPosts (req, res) {
+//     foo(req, res, 'categories', 'categoryid', renderCategoryPostsSlug)
+//   },
+
+module.exports = slugged
