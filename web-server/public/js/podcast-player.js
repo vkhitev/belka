a1a1c37628fe71dcffd486f8a411e80489c1ab83
Vlad Khitev <vkhitev@gmail.com>
2017-05-21 02:04:57 +0300

    Basic audio & slides sync

--- web-server/public/js/podcast-player.js
+++ web-server/public/js/podcast-player.js
@@ -1,28 +1,60 @@
-$(document).ready(function () {
-  $('.podcast-carousel').carousel({
-    interval: false
+const IntervalTree = require('interval-tree2')
+
+function disableLoopControls (carousel) {
+  const prev = carousel.children('.carousel-control-prev')
+  const next = carousel.children('.carousel-control-next')
+  const firstItem = carousel.find('.carousel-item:first')
+  const lastItem = carousel.find('.carousel-item:last')
+  carousel.on('slid.bs.carousel', e => {
+    if (firstItem.hasClass('active')) {
+      prev.hide()
+      next.show()
+    } else if (lastItem.hasClass('active')) {
+      prev.show()
+      next.hide()
+    } else {
+      prev.show()
+      next.show()
+    }
   })
-
-  $('.podcast-carousel').on('slid.bs.carousel', function () {
-    checkitem($(this).attr('id'))
-  })
-
-  $('.podcast-carousel').each(function () {
-    checkitem($(this).attr('id'))
-  })
-})
-
-function checkitem (identificator) {
-  var $this = $('#' + identificator)
-
-  if ($('#' + identificator + ' .carousel-item:first').hasClass('active')) {
-    $this.children('.carousel-control-prev').hide()
-    $this.children('.carousel-control-next').show()
-  } else if ($('#' + identificator + ' .carousel-item:last').hasClass('active')) {
-    $this.children('.carousel-control-next').hide()
-    $this.children('.carousel-control-prev').show()
-  } else {
-    $this.children('.carousel-control-prev').show()
-    $this.children('.carousel-control-next').show()
-  }
+  carousel.trigger('slid.bs.carousel')
 }
+
+function initCarousels () {
+  const carousels = Array.from($('[id^=podcast-carousel]')).map($)
+  carousels.forEach(disableLoopControls)
+  carousels.forEach(synchronizeAudio)
+}
+
+function createIntervalTree (root, marks) {
+  const itree = new IntervalTree(root)
+  for (let i = 0; i < marks.length - 1; i += 1) {
+    itree.add(marks[i], marks[i + 1], i)
+  }
+  return itree
+}
+
+function getValue (itree, key) {
+  return itree.search(key)[0].id
+}
+
+function synchronizeAudio (carousel, marks) {
+  const slider = carousel.find('[id^=podcast-slider]')
+  const audio = carousel.siblings('[id^=podcast-audio]')
+  audio.on('loadedmetadata', e => {
+    marks = [0, 2, 4, 200, 400, 600, 800, e.target.duration]
+    const itree = createIntervalTree(e.target.duration / 2, marks)
+
+    audio.on('seeked', onseeked)
+    audio.on('timeupdate', onseeked)
+
+    function onseeked (e) {
+      const slide = getValue(itree, e.target.currentTime)
+      slider.carousel(slide)
+    }
+  })
+}
+
+$(document).ready(function () {
+  initCarousels()
+})
